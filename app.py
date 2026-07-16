97bba03653077e252856680c7bb014f59be0b53a
Evettke09 <ivett.gamer9210@gmail.com>
2026-07-16 07:03:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -234,7 +234,7 @@ def parse_pdf(pdf_path):
 
 
     return transactions
-  import pandas as pd
+import pandas as pd
 from openpyxl import load_workbook
 from openpyxl.styles import Font, Alignment
 
